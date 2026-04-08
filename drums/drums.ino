0796d00a7f8ca50423fce14119e3052a30e33807
heesuch <juliehsuchung@gmail.com>
2026-04-07 20:20:07 -0400

    feat: new drum code

--- drums/drums.ino
+++ drums/drums.ino
@@ -1,8 +1,5 @@
 #include <Servo.h>
 
-Servo myservo;
-#include <Servo.h>
-
 Servo kickServo;
 Servo snareServo;
 Servo hihatServo;
@@ -11,26 +8,39 @@ const int kickPin = 9;
 const int snarePin = 10;
 const int hihatPin = 11;
 
-// BPM
-int bpm = 80;
+int bpm = 140;
 int beatInterval = 60000 / bpm;
 
+// Kick
 int kickRest = 20;
 int kickHit = 140;
 
-int snareRest = 30;
+// Snare
+int snareRest = 80;
 int snareHit = 110;
 
-int hihatRest = 40;
-int hihatHit = 90;
+// Hi-hat
+int hihatRest = 80;
+int hihatHit = 140;
 
 int kickHold = 40;
 int snareHold = 35;
-int hihatHold = 20;
+int hihatHold = 30;
 
 void hitKick() {
-  kickServo.write(kickHit);
+  for (int pos = kickRest; pos <= kickHit; pos += 8) {
+    kickServo.write(pos);
+    delay(4);
+  }
+
   delay(kickHold);
+
+  for (int pos = kickHit; pos >= kickRest; pos -= 8) {
+    kickServo.write(pos);
+    delay(4);
+  }
+
+  delay(20);
   kickServo.write(kickRest);
 }
 
@@ -41,12 +51,27 @@ void hitSnare() {
 }
 
 void hitHiHat() {
-  hihatServo.write(hihatHit);
+  // move up
+  for (int pos = hihatRest; pos <= hihatHit; pos += 5) {
+    hihatServo.write(pos);
+    delay(10);
+  }
+
   delay(hihatHold);
+
+  // move back down
+  for (int pos = hihatHit; pos >= hihatRest; pos -= 5) {
+    hihatServo.write(pos);
+    delay(10);
+  }
+
+  delay(50);
   hihatServo.write(hihatRest);
 }
 
 void setup() {
+  Serial.begin(9600);
+
   kickServo.attach(kickPin);
   snareServo.attach(snarePin);
   hihatServo.attach(hihatPin);
@@ -54,11 +79,16 @@ void setup() {
   kickServo.write(kickRest);
   snareServo.write(snareRest);
   hihatServo.write(hihatRest);
-  delay (500);
-  
+
+  delay(1000);
 }
 
 void loop() {
+  Serial.println("kick");
   hitKick();
   delay(beatInterval);
-  }
+
+  Serial.println("hihat");
+  hitHiHat();
+  delay(beatInterval);
+}
